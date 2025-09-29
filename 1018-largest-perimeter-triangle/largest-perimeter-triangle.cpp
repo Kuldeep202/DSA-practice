2d8d1b5641728c1dcb663dda14e025b375c3759e
Kuldeep202 <115531727+Kuldeep202@users.noreply.github.com>
2025-09-30 02:22:47 +0530

    Time: 4 ms (63.02%) | Memory: 25.7 MB (14.59%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -5,7 +5,7 @@ public:
         sort(arr.begin(),arr.end());
         for(int i=0; i<arr.size()-2; i++){
             int sum=arr[i]+arr[i+1];
-            if(sum>arr[i+2]&&arr[i]<=arr[i+1]&&arr[i+1]<=arr[i+2]){
+            if(sum>arr[i+2]){
                 
                 ans=max(ans,arr[i]+arr[i+1]+arr[i+2]);
             }

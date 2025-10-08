039af793bdfecdc5bf281f656afb014d0ca4c08a
Kuldeep202 <115531727+Kuldeep202@users.noreply.github.com>
2025-10-08 23:51:12 +0530

    Time: 51 ms (22.04%) | Memory: 128.2 MB (42.11%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        int n = potions.size();
+        vector<int> ans;
+        for (int spell : spells) {
+            long long minPotion = (success + spell - 1) / spell;
+            auto it = lower_bound(potions.begin(), potions.end(), minPotion);
+            int count = n - (it - potions.begin());
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};

358a03e7045a39e907e3e58bdf02c49347fbf920
kuldeep076 <115531727+kuldeep076@users.noreply.github.com>
2025-11-03 23:18:37 +0530

    Time: 306 ms (52.23%) | Memory: 96.7 MB (19.60%) - LeetSync

---
+++ 464-can-i-win/can-i-win.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canIWin(int maxc, int des) {
+        int full=(1<<(maxc))-1;
+        int maxsum=maxc*(maxc+1)/2;
+       if (maxsum<des)return false;
+        vector<array<int,2>>dp(full+1,{-1,-1});
+     
+        auto dfs=[&](auto self,int mask,int turn)->bool{
+            if (dp[mask][turn]!=-1)return dp[mask][turn];
+            bool res;
+            if (turn)res=true;
+            else res=false;
+            int allp=0;
+         
+            for(int i=1;i<=maxc;i++){
+                if ((mask&(1<<(i-1)))==0)continue;
+                allp+=i;
+            }
+            for(int i=1;i<=maxc;i++){
+                if (mask&(1<<(i-1)))continue;
+                if (allp+i>=des){
+                    if (turn)return dp[mask][turn]=false;
+                    return dp[mask][turn]=true;
+                }
+                else {
+                
+                    if (turn)res=res && self(self,mask|(1<<(i-1)),turn^1);
+               
+                    else res=res||self(self,mask|(1<<(i-1)),turn^1);
+                }
+            }
+            return dp[mask][turn]=res;
+        };
+        bool got=false;
+        for(int i=1;i<=maxc;i++){
+            if (i>=des)return true;
+            got=got || dfs(dfs,(1<<(i-1)),1);
+        }
+        return got;
+    }
+};
